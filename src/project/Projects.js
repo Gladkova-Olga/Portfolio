e83c64139519769ab02a26a531eafaf4af995282
Lisla Fox <lisla@Lislas-MBP.home>
2021-08-18 14:53:19 +0300

    projects

--- src/project/Projects.js
+++ src/project/Projects.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import style from './Projects.module.css';
+import style from "./Projects.module.css";
 import styleContainer from "../common/styles/Container.module.css";
 import ProjectItem from "./projectItem/ProjectItem";
 import Title from "../common/components/title/Title";
+import todoImage from "./../assets/images/todoList.jpeg"
+import socialNetworkImage from "./../assets/images/Social-network.jpeg"
 
 
 function Projects() {
+    const socialNetwork = {
+        backgroundImage: `url(${socialNetworkImage})`,
+    };
+    const todoList = {
+        backgroundImage: `url(${todoImage})`,
+    };
+
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
@@ -13,20 +22,22 @@ function Projects() {
 
                 <div className={style.projects}>
                 <ProjectItem
+                    style = {socialNetwork}
                     title = {"Social network"}
                     description = {"It's simple social network"}
                     pictureDescription = {"Social network"}
                 />
                     <ProjectItem
+                        style = {todoList}
                         title = {"TodoList"}
-                        description = {"Some description"}
+                        description = {"Some description ttttlj slkdfhs;kj dfhsjd fhgsljdfhgsl kjdfhglskdfj hgsd"}
                         pictureDescription = {"TodoList"}
                     />
-                    <ProjectItem
-                        title = {"Counter"}
-                        description = {"One more description"}
-                        pictureDescription = {"Counter"}
-                    />
+                    {/*<ProjectItem*/}
+                    {/*    title = {"Counter"}*/}
+                    {/*    description = {"One more description"}*/}
+                    {/*    pictureDescription = {"Counter"}*/}
+                    {/*/>*/}
                 </div>
 
             </div>
--- src/project/projectItem/ProjectItem.js
+++ src/project/projectItem/ProjectItem.js
@@ -2,18 +2,19 @@ import React from 'react';
 import style from './ProjectItem.module.css';
 
 
-
 function ProjectItem(props) {
     return (
         <div className={style.projectItem}>
-            <div className={style.pictureContainer}>
+            <div className={style.pictureContainer} style={props.style}>
                 {/*<img href="" alt={props.pictureDescription}/>*/}
                 <a className={style.projectButton}>Look</a>
+
             </div>
-            <h6 className={style.projectTitle}>{props.title}</h6>
-            <span className={style.projectText}>
-                {props.description}
-            </span>
+            <div className={style.projectInfo}>
+                <h6 className={style.projectTitle}>{props.title}</h6>
+                <p className={style.projectText}>{props.description}</p>
+            </div>
+
         </div>
     );
 }
