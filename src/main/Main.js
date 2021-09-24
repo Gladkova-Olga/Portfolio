09a119d3ac0432d964bec8507b033661254b6a8c
Lisla Fox <lisla@Lislas-MBP.home>
2021-09-24 15:57:29 +0300

    main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import style from './Main.module.css';
-import styleContainer from './../common/styles/Container.module.css'
+import style from './Main.module.scss';
+import styleContainer from './../common/styles/Container.module.css';
+import meImg from './../assets/images/me.jpg'
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.text}>
                     <span>Hi there!</span>
-                    <h1>I'm Olga Gladkova</h1>
-                    <p>A frontend developer</p>
+                    <h4 className={style.title}>I'm Olga Gladkova</h4>
+                    <p className={style.description}>A frontend developer</p>
                 </div>
-                <div className={style.photo}></div>
+                <div >
+                    <img className={style.photo}src = {meImg} alt={""}/>
+                </div>
             </div>
         </div>
     );
