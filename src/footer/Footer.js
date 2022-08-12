ffd45b690c54f00fee3a018b9409fd2933407ada
Lisla Fox <lisla@Lislas-MBP.home>
2022-08-12 17:00:08 +0300

    fixed footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -4,6 +4,7 @@ import FooterItem from "./FooterItem/FooterItem";
 import {faPhone} from '@fortawesome/free-solid-svg-icons'
 import {faAt} from '@fortawesome/free-solid-svg-icons'
 import {faLocationArrow} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 
 function Footer() {
@@ -11,9 +12,14 @@ function Footer() {
         <div className={style.footer}>
             <div className={` ${style.footerContainer}`}>
                 <div className={style.footerItems}>
-                    <FooterItem icon = {faAt} title = "E-mail" content = "olga_gladkova@tut.by"/>
-                    <FooterItem icon = {faPhone} title = "Phone" content = "+37061942746"/>
-                    <FooterItem icon = {faLocationArrow} title = "City" content = "Vilnius"/>
+                    <FooterItem icon={faAt} title="E-mail" content="volha.gladkova@gmail.com"/>
+                    <FooterItem icon={faPhone} title="Phone" content="+37061942746"/>
+                    <FooterItem icon={faLocationArrow} title="City" content="Vilnius"/>
+                    <a href="https://www.linkedin.com/in/olga-gladkova-202294242/">
+                        <FooterItem icon={faLinkedin} title="LinkedIn" content=""/>
+                    </a>
+
+
                 </div>
                 <div className={style.footerText}>&#169; And made with love All rights reserved</div>
             </div>
