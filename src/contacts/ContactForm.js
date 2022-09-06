3f2bc8e4d288dc793b61f5e5e20be3db7ea8a52b
Lisla Fox <lisla@Lislas-MBP.home>
2022-09-06 13:38:38 +0300

    contact changed

--- src/contacts/ContactForm.js
+++ src/contacts/ContactForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Contact.module.scss';
 import Title from "../common/components/title/Title";
+import Button from "../common/components/button/Button";
 import Flip from 'react-reveal/Flip';
 import FooterItem from "../footer/FooterItem/FooterItem";
 import {faAt, faLocationArrow, faPhone} from "@fortawesome/free-solid-svg-icons";
@@ -13,14 +14,12 @@ function Contact() {
                 <Flip top>
                     <Title text={"Contact"}/>
                 </Flip>
-                <div className={style.contactsItems}>
-                    <FooterItem icon={faAt} title="E-mail" content="volha.gladkova@gmail.com"/>
-                    <FooterItem icon={faPhone} title="Phone" content="+37061942746"/>
-                    <FooterItem icon={faLocationArrow} title="City" content="Vilnius"/>
-                    <a href="https://www.linkedin.com/in/olga-gladkova-202294242/">
-                        <FooterItem icon={faLinkedin} title="LinkedIn" content=""/>
-                    </a>
-                </div>
+                <form className={style.form}>
+                    <input type="text" name="name" placeholder="Name"/>
+                    <input type="text" name="e-mail" placeholder="E-mail"/>
+                    <textarea name="message" placeholder="Your message"/>
+                    <Button buttonType={"submit"} buttonName="Send message"/>
+                </form>
 
             </div>
         </div>
