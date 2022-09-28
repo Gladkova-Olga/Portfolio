1e9d57cbb7bf4135e972f3d1afcd854877019f19
Lisla Fox <lisla@Lislas-MBP.home>
2022-09-28 14:10:39 +0300

    new images

--- src/project/Projects.js
+++ src/project/Projects.js
@@ -2,8 +2,9 @@ import React from 'react';
 import style from "./Projects.module.scss";
 import ProjectItem from "./projectItem/ProjectItem";
 import Title from "../common/components/title/Title";
-import todoImage from "./../assets/images/todoList.jpeg"
-import socialNetworkImage from "./../assets/images/Social-network.jpeg"
+import todoImage from "./../assets/images/ToDoList.png"
+import socialNetworkImage from "./../assets/images/SocialNetwork.png"
+import cardsImage from "./../assets/images/Cards.png"
 import Flip from 'react-reveal/Flip';
 
 
@@ -14,6 +15,9 @@ function Projects() {
     const todoList = {
         backgroundImage: `url(${todoImage})`,
     };
+    const cards = {
+        backgroundImage: `url(${cardsImage})`,
+    };
     const projectItems = [
         {
             style: socialNetwork,
@@ -21,7 +25,7 @@ function Projects() {
             description: "This is a simple social network. It was is my learning project." +
                 "I used Typescript, React, Redux, SCSS. This project was developed by applying of " +
                 "React class based components. " +
-                "Also I used such libraries like axios, redux-thunk. ",
+                "Also I used such libraries like redux-thunk, axios. ",
             projectLink: "https://github.com/Gladkova-Olga/SocialNetwork"
         },
         {
@@ -30,8 +34,17 @@ function Projects() {
             description: "I used Typescript, React, Redux for developing the To Do List. " +
                 "You can add, change, delete your tasks at different lists. It was a learning project. " +
                 "It was developed by applying of React functional components. " +
-                "I used axios, formic, material-ui, redux-thunk.",
+                "I used axios, material-ui, redux-thunk, formic.",
             projectLink: "https://github.com/Gladkova-Olga/MyTodoList"
+        },
+        {
+            style: cards,
+            title: "Cards",
+            description: "Cards is application which help to learn whatever you want. " +
+                "You can make your own packs or use packs of other users, sort or delete them. " +
+                "For every card you can grade your answer. I used Typescript, React, Redux. " +
+                "It was developed by applying of React functional components. I used axios, formic, redux-thunk.",
+            projectLink: "https://github.com/Gladkova-Olga/Cards"
         }
     ]
 
